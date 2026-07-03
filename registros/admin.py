375b9f5cfcf1d7894be67afa11eae3c9dc459f14
Kevin Alejo <kevin11matiasalejo@gmail.com>
2026-07-03 10:53:15 -0600

    Mi primer commit

---
+++ registros/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Alumnos
+from .models import Comentario
+from .models import ComentarioContacto
+# Register your models here.
+
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=('matricula','nombre','carrera','turno')
+    list_editable = ('turno',)
+    search_fields = ('matricula','nombre','carrera','turno')
+    date_hierarchy = 'created'
+    list_filter = ('carrera','turno')
+admin.site.register(Alumnos,AdministrarModelo)
+
+class AdministrarComentarios(admin.ModelAdmin):
+    list_display=('id','coment','alumno')
+    search_fields=('id','created')
+    date_hierarchy='created'
+    readonly_fields=('created','id')
+admin.site.register(Comentario,AdministrarComentarios)
+
+
+class AdministrarComentariosContacto(admin.ModelAdmin):
+    list_display = ('id', 'mensaje')
+    search_fields = ('id','created')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+admin.site.register(ComentarioContacto, AdministrarComentariosContacto)
